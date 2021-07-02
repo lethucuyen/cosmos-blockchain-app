30c53189bec10416dd1317f00601cad636602c23
tanhahaha123 <tanhahaha123.mail@gmail.com>
2021-07-02 15:08:18 +0000

    First Commit

---
+++ vue/src/store/generated/user/planet/user.planet.blog/module/index.js
@@ -0,0 +1,54 @@
+// THIS FILE IS GENERATED AUTOMATICALLY. DO NOT MODIFY.
+import { SigningStargateClient } from "@cosmjs/stargate";
+import { Registry } from "@cosmjs/proto-signing";
+import { Api } from "./rest";
+import { MsgCreateTimedoutPost } from "./types/blog/tx";
+import { MsgDeleteSentPost } from "./types/blog/tx";
+import { MsgUpdateTimedoutPost } from "./types/blog/tx";
+import { MsgCreateSentPost } from "./types/blog/tx";
+import { MsgDeletePost } from "./types/blog/tx";
+import { MsgSendIbcPost } from "./types/blog/tx";
+import { MsgUpdateSentPost } from "./types/blog/tx";
+import { MsgUpdatePost } from "./types/blog/tx";
+import { MsgDeleteTimedoutPost } from "./types/blog/tx";
+import { MsgCreatePost } from "./types/blog/tx";
+const types = [
+    ["/user.planet.blog.MsgCreateTimedoutPost", MsgCreateTimedoutPost],
+    ["/user.planet.blog.MsgDeleteSentPost", MsgDeleteSentPost],
+    ["/user.planet.blog.MsgUpdateTimedoutPost", MsgUpdateTimedoutPost],
+    ["/user.planet.blog.MsgCreateSentPost", MsgCreateSentPost],
+    ["/user.planet.blog.MsgDeletePost", MsgDeletePost],
+    ["/user.planet.blog.MsgSendIbcPost", MsgSendIbcPost],
+    ["/user.planet.blog.MsgUpdateSentPost", MsgUpdateSentPost],
+    ["/user.planet.blog.MsgUpdatePost", MsgUpdatePost],
+    ["/user.planet.blog.MsgDeleteTimedoutPost", MsgDeleteTimedoutPost],
+    ["/user.planet.blog.MsgCreatePost", MsgCreatePost],
+];
+const registry = new Registry(types);
+const defaultFee = {
+    amount: [],
+    gas: "200000",
+};
+const txClient = async (wallet, { addr: addr } = { addr: "http://localhost:26657" }) => {
+    if (!wallet)
+        throw new Error("wallet is required");
+    const client = await SigningStargateClient.connectWithSigner(addr, wallet, { registry });
+    const { address } = (await wallet.getAccounts())[0];
+    return {
+        signAndBroadcast: (msgs, { fee = defaultFee, memo = null }) => memo ? client.signAndBroadcast(address, msgs, fee, memo) : client.signAndBroadcast(address, msgs, fee),
+        msgCreateTimedoutPost: (data) => ({ typeUrl: "/user.planet.blog.MsgCreateTimedoutPost", value: data }),
+        msgDeleteSentPost: (data) => ({ typeUrl: "/user.planet.blog.MsgDeleteSentPost", value: data }),
+        msgUpdateTimedoutPost: (data) => ({ typeUrl: "/user.planet.blog.MsgUpdateTimedoutPost", value: data }),
+        msgCreateSentPost: (data) => ({ typeUrl: "/user.planet.blog.MsgCreateSentPost", value: data }),
+        msgDeletePost: (data) => ({ typeUrl: "/user.planet.blog.MsgDeletePost", value: data }),
+        msgSendIbcPost: (data) => ({ typeUrl: "/user.planet.blog.MsgSendIbcPost", value: data }),
+        msgUpdateSentPost: (data) => ({ typeUrl: "/user.planet.blog.MsgUpdateSentPost", value: data }),
+        msgUpdatePost: (data) => ({ typeUrl: "/user.planet.blog.MsgUpdatePost", value: data }),
+        msgDeleteTimedoutPost: (data) => ({ typeUrl: "/user.planet.blog.MsgDeleteTimedoutPost", value: data }),
+        msgCreatePost: (data) => ({ typeUrl: "/user.planet.blog.MsgCreatePost", value: data }),
+    };
+};
+const queryClient = async ({ addr: addr } = { addr: "http://localhost:1317" }) => {
+    return new Api({ baseUrl: addr });
+};
+export { txClient, queryClient, };
